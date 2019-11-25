10427e258b9c0d893ef4b6c350e836fccc6852d9
jinkyuhan <gkswlsrb95@gmail.com>
2019-11-25 15:45:15 +0900

    feat: Add connection to MySQL DB server

--- back/app.js
+++ back/app.js
@@ -4,13 +4,38 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var somethingApiRouter = require('./api/something');
+var mysql = require('mysql');
+
 
 var app = express();
 
 
 
-// Database
-/* mySQL connection init */
+// Connection to Database
+var connection = mysql.createConnection({
+	host: 'mysqldb.cv9byhmyfmai.us-east-1.rds.amazonaws.com',
+	user: 'admin',
+	password: '12345678',
+	port: 3306,
+	database: 'DOONG_JI'
+});
+connection.connect(function(err){
+	console.log('try Connecting to mySQL DB server...')
+	if(err){
+		console.error(err);
+	} else {
+		console.log('mySQL DB server successful connected');
+	}
+});
+
+// var sql = `SELECT * FROM init`;
+// connection.query(sql, function(err, rows, fields){
+// 	if(!err){
+// 		console.log(rows);
+// 	} else {
+// 		console.log('query error');
+// 	}
+// });
 
 
 // Middlewares

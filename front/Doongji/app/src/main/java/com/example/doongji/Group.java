230b615ed9575ea8f90ccf27ae64162719bfd7ea
hgh1146 <45303105+hgh1146@users.noreply.github.com>
2019-11-27 22:44:01 +0900

    feat : Add Layout2

--- front/Doongji/app/src/main/java/com/example/doongji/Group.java
+++ front/Doongji/app/src/main/java/com/example/doongji/Group.java
@@ -1,7 +1,11 @@
 package com.example.doongji;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -17,13 +21,48 @@ public class Group extends AppCompatActivity {
     public void onClickButton(View v) {
         switch (v.getId()) {
             case R.id.Option_btn:
-                Intent i1 = new Intent(Group.this, Create_group.class);
-                startActivity(i1);
+                //OpenDrawer 출처 https://g-y-e-o-m.tistory.com/128
+                DrawerLayout drawer = findViewById(R.id.drawer_layout);
+                drawer.openDrawer(GravityCompat.END);
                 break;
             case R.id.test2_btn:
-                Intent i2 = new Intent(Group.this, Message.class);
+                Intent i1 = new Intent(Group.this, Message.class);
+                startActivity(i1);
+                break;
+            case R.id.Add_member_btn:
+                Intent i2 = new Intent(Group.this, Add_member.class);
                 startActivity(i2);
                 break;
+            case R.id.Group_setting_btn:
+                Intent i3 = new Intent(Group.this, Set_group.class);
+                startActivity(i3);
+                break;
+            case R.id.Group_exit_btn:
+                new AlertDialog.Builder(this)
+                        .setTitle("둥지 나가기")
+                        .setMessage("둥지를 나가시겠습니까?")
+                        .setPositiveButton("예", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface arg0, int arg1) {
+
+                                    }
+                                }
+                        )
+                        .setNegativeButton("아니요", null)
+                        .setCancelable(false)
+                        .show();
+                break;
+        }
+    }
+
+    //이전 버튼 클릭시 Drawer 닫기 출처 https://lktprogrammer.tistory.com/168
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
         }
     }
 }
--- front/Doongji/app/src/main/java/com/example/doongji/MainActivity.java
+++ front/Doongji/app/src/main/java/com/example/doongji/MainActivity.java
@@ -174,4 +174,5 @@ public class MainActivity extends AppCompatActivity {
 
         }.execute();
     }
+
 }

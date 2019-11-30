bcf679694fd67ec687883bf3dca94db4bcc12052
hgh1146 <45303105+hgh1146@users.noreply.github.com>
2019-11-30 11:08:45 +0900

    docs : Fix Layout

--- front/Doongji/app/src/main/java/com/example/doongji/Login.java
+++ front/Doongji/app/src/main/java/com/example/doongji/Login.java
@@ -21,7 +21,7 @@ public class Login extends AppCompatActivity {
     private String Pw;
 
     //    private EditText id;
-//    private EditText pw;
+    //    private EditText pw;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,41 +29,53 @@ public class Login extends AppCompatActivity {
     }
 
     public void onClickButton(View view) {
-        EditText id = (EditText) findViewById(R.id.ID_input);
-        EditText pw = (EditText) findViewById(R.id.PW_input);
-        ArrayList<String> array = new ArrayList<>();
-        array.add(id.getText().toString());
-        array.add(pw.getText().toString());
 
-        class MyRunnable implements Runnable {
-            ArrayList<String> param;
-
-            public MyRunnable(ArrayList<String> parameter) {
-                this.param = parameter;
-            }
-
-            public void run() {
-                HttpConnection connecter = new HttpConnection(getString(R.string.IPAd));
-                try {
-
-                    Log.i("LoginInfo", "Id: " + param.get(0) + "Pw:" + param.get(1));
-                    results = connecter.sendGet("/api/members?user_id=" + param.get(0) + "&user_pw=" + param.get(1));
-                    if (results.length() != 0) {
-                        Intent i = new Intent(Login.this, Group_index.class);
-                        startActivity(i);
-                    } else {
-                        TextView textView=(TextView)findViewById(R.id.textView);
-                        textView.setText("로그인 실패\n다시 시도하세요.!!!!!");
-                    }
-
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
+//        EditText id = (EditText) findViewById(R.id.ID_input);
+//        EditText pw = (EditText) findViewById(R.id.PW_input);
+//        ArrayList<String> array = new ArrayList<>();
+//        array.add(id.getText().toString());
+//        array.add(pw.getText().toString());
+        switch (view.getId()) {
+            case R.id.Sign_up_btn:
+                Intent i1 = new Intent(Login.this, Sign_up.class);
+                startActivity(i1);
+                break;
+            case R.id.Log_in_btn:
+                Intent i2 = new Intent(Login.this, Group_index.class);
+                startActivity(i2);
+//                class MyRunnable implements Runnable {
+//                    ArrayList<String> param;
+//
+//                    public MyRunnable(ArrayList<String> parameter) {
+//                        this.param = parameter;
+//                    }
+//
+//                    public void run() {
+//                        HttpConnection connecter = new HttpConnection(getString(R.string.IPAd));
+//                        try {
+//
+//                            Log.i("LoginInfo", "Id: " + param.get(0) + "Pw:" + param.get(1));
+//                            results = connecter.sendGet("/api/members?user_id=" + param.get(0) + "&user_pw=" + param.get(1));
+//                            if (results.length() != 0) {
+//                                Intent i = new Intent(Login.this, Group_index.class);
+//                                startActivity(i);
+//                            } else {
+//                                TextView textView = (TextView) findViewById(R.id.textView);
+//                                textView.setText("로그인 실패\n다시 시도하세요.!!!!!");
+//                            }
+//
+//                        } catch (Exception e) {
+//                            e.printStackTrace();
+//                        }
+//                    }
+//                }
+//
+//                Runnable r = new MyRunnable(array);
+//                new Thread(r).start();
+                break;
         }
 
-        Runnable r = new MyRunnable(array);
-        new Thread(r).start();
+
 
     }
 }

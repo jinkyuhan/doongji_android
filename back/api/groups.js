fdf5d48cdd6ae400c706934f5a59fa197afe4a24
jinkyuhan <gkswlsrb95@gmail.com>
2019-11-30 06:17:22 +0000

    refactor: Add error exception handling to member api

--- back/api/groups.js
+++ back/api/groups.js
@@ -1,9 +1,35 @@
 var express = require('express');
 var router = express.Router();
+var models = require('../models/models');
+const Sequelize = require('sequelize');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+const Op = Sequelize.Op;
+/*CREATE*/
+/* POST NEW MEMBER */
+router.put('/', async function(req, res, next) {});
+
+/* GET GROUPS */
+router.get('/', async function(req, res, next) {
+	// BY NONE (ALL)
+	try {
+		var groups = await models.Group.findAll();
+		res.json(groups);
+		console.log(`response : ${groups}`);
+	} catch (err) {
+		console.log(`GET GROUP ERROR : ${err}`);
+	}
+});
+
+/*GET A MEMBER BY GROUP ID*/
+router.get('/:id', async function(req, res, next) {
+	try {
+		var theMember = await models.Group.findOne({
+			where: { grp_id: String(req.params.id) }
+		});
+	} catch (err) {
+		console.log(`GET A MEMBER BY GROUP ID ERROR : ${err}`);
+	}
+	res.json(theMember);
 });
 
 module.exports = router;
--- back/api/members.js
+++ back/api/members.js
@@ -4,43 +4,66 @@ var models = require('../models/models');
 const Sequelize = require('sequelize');
 
 const Op = Sequelize.Op;
+
 /*CREATE*/
+
 /* POST NEW MEMBER */
-router.put('/', async function (req, res, next) {
-
-
+router.post('/', async function(req, res, next) {
+	
 });
 
+/*READ*/
 /* GET MEMBERS */
-router.get('/', async function (req, res, next) {
-	console.log(Op);
+router.get('/', async function(req, res, next) {
 	if (req.query.user_id !== undefined && req.query.user_pw !== undefined) {
-		 models.Member.findAll({
-			where: {
-				user_id: {[Op.eq] : String(req.query.user_id)}, 
-				user_pw: {[Op.eq] : String(req.query.user_pw)}
-			}
-		}).then((members)=>{
+		// BY ID & PW
+		try {
+			var members = await models.Member.findAll({
+				where: {
+					user_id: { [Op.eq]: String(req.query.user_id) },
+					user_pw: { [Op.eq]: String(req.query.user_pw) }
+				}
+			});
 			res.json(members);
-			console.log(`!!!!!!!response : ${members}`);
-		});
-	} else if(req.query.user_name !== undefined){
-
+			console.log(`response : ${members}`);
+		} catch (err) {
+			console.log(`GET MEMBERS (by user_id & user_pw) ERROR!!! : ${err}`);
+		}
 	} else {
-		models.Member.findAll()
-		.then((members)=>{
+		// else if (req.query.user_name !== undefined) {
+		// 	// BY NAME
+		// 	try {
+		// 		var theMember = await models.Members.findAll({
+		// 			where: { user_name: String(req.query.user_name) }
+		// 		});
+		// 		res.json(theMember);
+		// 	} catch (err) {
+		// 		console.log(`GET MEMBERS (by name) ERROR!!! : ${err} `);
+		// 	}
+		// }
+		// BY NONE (ALL)
+		try {
+			var members = await models.Member.findAll();
 			res.json(members);
-			console.log(`!!!!!!response : ${members}`);
-		});
+			console.log(`response : ${members}`);
+		} catch (err) {
+			console.log(`GET ALL MEMBERS ERROR!!! : ${err} `);
+		}
 	}
 });
 
 /*GET A MEMBER BY ID*/
-router.get('/:id', async function (req, res, next) {
-	var theMember = await models.Member.findOne({
-		where: { mem_id: String(req.params.id) }
-	});
-	res.json(theMember);
+router.get('/:id', async function(req, res, next) {
+	// BY ID
+	try {
+		var theMember = await models.Member.findOne({
+			where: { mem_id: String(req.params.id) }
+		});
+		console.log(theMember);
+		res.json(theMember);
+	} catch (err) {
+		console.log(`GET THE MEMBER (BY ID) ERROR!!! : ${res}`);
+	}
 });
 
 module.exports = router;

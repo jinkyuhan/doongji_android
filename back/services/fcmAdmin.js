bf533d3bd9128226d8cb337330d9be7f2f381b8c
jinkyuhan <gkswlsrb95@gmail.com>
2019-12-15 03:50:15 +0900

    feat: Add fcmAdmin module

---
+++ back/services/fcmAdmin.js
@@ -0,0 +1,27 @@
+var admin = require('firebase-admin');
+var serviceAccount = require('./doongji-e3b61-firebase-adminsdk-4jwxq-2282b41d35');
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
+
+admin.sendMessageToTopic = async function (_title, _message, _topic) {
+	var message = {
+		notification: {
+			title: _title,
+			body: _message
+		},
+		topic: _topic
+	};
+
+	try {
+		result = admin.messaging().send(message);
+		result.then(response =>
+			console.log(`Successfully sent message: ${response}`));
+
+	} catch (err) {
+		console.log(`Erorr sending message: ${err}`);
+	}
+}
+
+module.exports = admin;
--- back/services/sentry.js
+++ back/services/sentry.js
@@ -1,20 +1,23 @@
 var express = require('express');
 var router = express.Router();
 var models = require('../models/models');
+var admn = require('./fcmAdmin');
 const Sequelize = require('sequelize');
 const Op = Sequelize.Op;
 
 
-router.post('/:user_id/accessto/:grp_id',async function (req, res, next){
+
+router.post('/:user_id/accessto/:grp_id', async function (req, res, next) {
+	console.log("hihi");
 	//message
-	var yourMessage = await models.message
+	// var yourMessage = await models.message
+	admn.sendMessageToTopic('제목입니다', '내용입니다.', 'test_group2');
 	// notification
-	var others = await models.Belongs_to.findAll({
-		where : {
-			grp_id : req.params.grp_id
-		}
-	});
-	console.log('hihihi');
+	// var others = await models.Belongs_to.findAll({
+	// 	where : {
+	// 		grp_id : req.params.grp_id
+	// 	}
+	// });	
 });
 
 module.exports = router;
